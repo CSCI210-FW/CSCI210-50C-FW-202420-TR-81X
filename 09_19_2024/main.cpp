eeda614f8bd0d49441bcda89cce64dfc3d4b2f12
Angela <avenable11@ivytech.edu>
2024-09-19 19:04:18 -0400

    1st prepared statement

--- 09_19_2024/main.cpp
+++ 09_19_2024/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include <sqlite3.h>
+#include <limits>
+
+void viewAssignmentsByProject(sqlite3 *);
+void resetStream();
 
 int main()
 {
@@ -16,7 +20,28 @@ int main()
     {
         std::cout << "Database opened successfully." << std::endl;
     }
-
+    viewAssignmentsByProject(db);
     sqlite3_close(db);
     return 0;
+}
+
+void resetStream()
+{
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void viewAssignmentsByProject(sqlite3 *db)
+{
+    std::string query;
+    sqlite3_stmt *result;
+    query = "select proj_num, proj_name from project";
+    int rc = sqlite3_prepare_v2(db, query.c_str(), -1, &result, NULL);
+    if (rc != SQLITE_OK)
+    {
+        sqlite3_finalize(result);
+        std::cout << "There was an error with the project query: " << sqlite3_errmsg(db) << std::endl;
+        std::cout << query << std::endl;
+        return;
+    }
 }
